d6cd0da585db6121d661c65e0c8e63296d168872
andriii-droid <andri.andermatt@icloud.com>
2026-05-06 14:38:13 +0200

    mock sends three bytes of data when polled

--- chips/i2c-mock.chip.c
+++ chips/i2c-mock.chip.c
@@ -12,6 +12,48 @@ typedef struct
     pin_t pin_sda;
 } chip_state_t;
 
+
+
+typedef struct
+{
+    uint8_t items[100];
+    int top;
+} Stack;
+
+Stack dataStack;
+
+// Initialize stack
+void initStack(Stack *s)
+{
+    s->top = -1; // -1 means the stack is empty
+}
+
+void push(Stack *s, uint8_t data)
+{
+    if (s->top == 100 - 1)
+    {
+        printf("Stack Overflow! Cannot push %d\n", data);
+    }
+    else
+    {
+        s->items[++(s->top)] = data;
+        printf("Pushed %d onto the stack.\n", data);
+    }
+}
+
+uint8_t pop(Stack *s)
+{
+    if (s->top == -1)
+    {
+        printf("Stack Underflow! The stack is empty.\n");
+        return -1; // Error value
+    }
+    else
+    {
+        return s->items[(s->top)--];
+    }
+}
+
 static bool on_i2c_connect(void *user_data, uint32_t address, bool connect);
 static uint8_t on_i2c_read(void *user_data);
 static bool on_i2c_write(void *user_data, uint8_t data);
@@ -32,6 +74,12 @@ void chip_init()
         .disconnect = on_i2c_disconnect, // Optional
     };
     i2c_init(&i2c_config);
+
+    initStack(&dataStack);
+
+    push(&dataStack, 44);
+    push(&dataStack, 0);
+    push(&dataStack, 0);
 }
 
 bool on_i2c_connect(void *user_data, uint32_t address, bool connect)
@@ -41,9 +89,11 @@ bool on_i2c_connect(void *user_data, uint32_t address, bool connect)
 
 uint8_t on_i2c_read(void *user_data) //When Data requested
 {
-    printf("Sending byte to controller: ");
-    printf("%x\n", 0x01);
-    return 0x01;
+    uint8_t data = pop(&dataStack);
+    printf("Sending data from controller: ");
+    printf("%x", data);
+
+    return data;
 }
 
 bool on_i2c_write(void *user_data, uint8_t data)
